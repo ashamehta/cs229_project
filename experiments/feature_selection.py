8b6d782bf73859c3dedd1a368afc57ce9b5f2710
AntzenSb <83480742+AntzenSb@users.noreply.github.com>
2021-05-05 21:55:11 -0700

    Corrected bugs/typos in feature_selection script

--- experiments/feature_selection.py
+++ experiments/feature_selection.py
@@ -1,26 +1,29 @@
 
+import pandas as pd
+
 import numpy as np
 import sklearn as sk
-form sklearn import feature_selection
+from sklearn import feature_selection
 
 import matplotlib.pyplot as plt
 
 
-def remove_low_variance_features(feature_df, quantile=0.85, histogram_name="variance"):
+def remove_low_variance_features(feature_df, quantile=0.85, features_name="feature"):
     variances = np.var(np.asarray(feature_df)[:,1:], axis=0)
     quant = np.quantile(variances, quantile)
 
-    plt.hist(variances)
+    plt.hist(variances, bins=50)
     plt.yscale("log")
-    plt.title("Histogram of Feature Variances")
+    plt.title("Histogram of " + features_name + " variances\n(red line is 85th percentile)")
     plt.xlabel("Variance of Feature")
     plt.ylabel("Num features with specified variance (log)")
-    plt.savefig(variance + ".png")
+    plt.vlines(quant, colors="red", ymin=0, ymax=plt.gca().get_ylim()[1])
+    plt.savefig(features_name + "_variance.png")
 
-    sel = VarianceThreshold(threshold=quant)
+    sel = feature_selection.VarianceThreshold(threshold=quant)
     sel.fit(feature_df.drop(columns=["case_id"]))
     retained_indeces = sel.get_support(indices=True)
-    new_feature_df = feature_df.iloc[:,retained_indices]
+    new_feature_df = feature_df.iloc[:,retained_indeces]
 
     return new_feature_df
 
@@ -31,5 +34,5 @@ gexp_tsv = "processed_data/gene_expression_matrix.tsv"
 clinical_tsv = "processed_data/clinical_processed.tsv"
 
 raw_mut_df = pd.read_csv(mutation_tsv, sep="\t")
-mut_df = remove_low_variance_features(raw_mut_df, histogram_name="mutations_variance")
+mut_df = remove_low_variance_features(raw_mut_df, features_name="mutation_feature")
 
